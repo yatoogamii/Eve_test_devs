ce4e6a0b3cbe047d9861e5990902b564ad7719be
yatoogamii <bracaval.elias.pro@gmail.com>
2020-05-13 12:36:03 +0200

    add interfaces folder with interfaces of Events

---
+++ app/interfaces/IEvent.ts
@@ -0,0 +1,33 @@
+// firebase types
+import {FirebaseFirestoreTypes} from '@react-native-firebase/firestore';
+
+export interface IEvent {
+  eventId: string;
+  icon: string;
+  name: string;
+  location: {
+    city: string;
+    place: string;
+  };
+  participants: FirebaseFirestoreTypes.DocumentReference[];
+  needCar: boolean;
+  startAt: number;
+  endAt: number | null;
+  createdBy: FirebaseFirestoreTypes.DocumentReference;
+  todos: FirebaseFirestoreTypes.DocumentReference[];
+}
+
+export interface IEvents {
+  [key: string]: IEvent[];
+}
+
+export interface ITodo {
+  todoId: string;
+  eventRef: FirebaseFirestoreTypes.DocumentReference;
+  icon: string;
+  name: string;
+  personsAssigned: {
+    finished: boolean;
+    userRef: FirebaseFirestoreTypes.DocumentReference;
+  }[];
+}
