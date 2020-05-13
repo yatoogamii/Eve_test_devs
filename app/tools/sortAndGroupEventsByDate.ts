d85f2018c43bb1d3ca3bfb7e8bb3d023dd4f871c
yatoogamii <bracaval.elias.pro@gmail.com>
2020-05-13 15:27:32 +0200

    [refactoring] - use momentjs to format Unix timestamps to local string

--- app/tools/sortAndGroupEventsByDate.ts
+++ app/tools/sortAndGroupEventsByDate.ts
@@ -1,15 +1,39 @@
 // lodash
 import groupBy from 'lodash/groupBy';
+import cloneDeep from 'lodash/cloneDeep';
+
+// momentjs
+import moment from 'moment';
 
 // interfaces
 import {IEvent, IEvents} from '../interfaces/IEvent';
 
-export const sortAndGroupEventsByDate = (events: IEvent[]): IEvents => {
-  return groupBy(events, getDate);
-};
+export const sortAndGroupEventsByDate = (events: IEvent[]): IEvents =>
+  formatUnixTimestamps(groupBy(events, getDate));
 
 const getDate = (event: IEvent) => {
-  return `${new Date(event.startAt * 1000).getDate()}_${new Date(
-    event.startAt * 1000,
-  ).getMonth()}_${new Date(event.startAt * 1000).getFullYear()}`;
+  return moment
+    .unix(+event.startAt)
+    .utc()
+    .format('DD_MM_YYYY');
+};
+
+const formatUnixTimestamps = (eventsGroupByDate: IEvents) => {
+  for (const date in eventsGroupByDate) {
+    eventsGroupByDate[date].forEach((event: IEvent) => {
+      event.startAt = moment
+        .unix(+event.startAt)
+        .utc()
+        .format('HH : mm');
+      event.endAt =
+        event.endAt === null
+          ? null
+          : moment
+              .unix(+event.endAt)
+              .utc()
+              .format('HH : mm');
+    });
+  }
+
+  return eventsGroupByDate;
 };
