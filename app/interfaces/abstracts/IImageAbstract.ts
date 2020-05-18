951927b9a7f6b5801930ec590e7ff3e8bb39508a
yatoogamii <bracaval.elias.pro@gmail.com>
2020-05-18 19:55:29 +0200

    update IImageAbstract interface to add margin and padding helper

--- app/interfaces/abstracts/IImageAbstract.ts
+++ app/interfaces/abstracts/IImageAbstract.ts
@@ -2,5 +2,13 @@ export interface IImageAbstract {
   width?: string;
   height?: string;
   radius?: string;
-  uri?: string;
+  source?: any;
+  pTop?: string;
+  pRight?: string;
+  pBottom?: string;
+  pLeft?: string;
+  mTop?: string;
+  mRight?: string;
+  mBottom?: string;
+  mLeft?: string;
 }
